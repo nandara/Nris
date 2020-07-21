b1cfebd04bd4e1a54ee89c88a0a596664f0e748c
Nris07 <61815850+Nris07@users.noreply.github.com>
2020-07-21 11:42:15 +0700

    Update vc2.php

--- vc2.php
+++ vc2.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
-include "function2.php";
+include "function1.php";
 echo color("white","Token: ");
 $token = trim(fgets(STDIN));
 echo "\n".color("white","Claim?: y/n ");
