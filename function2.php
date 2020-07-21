90be61597c4e762af578309c55ec7cbab84d980c
Nris07 <61815850+Nris07@users.noreply.github.com>
2020-07-21 12:16:03 +0700

    Update function2.php

--- function2.php
+++ function2.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.53.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.886337,107.542217";
+$header[] = "X-Location: -5.166209,119.444146";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
