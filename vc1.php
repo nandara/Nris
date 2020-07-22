56627aa03c12e5c5c9d1adecaccf21dd4fca54fa
Nris07 <61815850+Nris07@users.noreply.github.com>
2020-07-22 19:04:33 +0700

    Update vc1.php

--- vc1.php
+++ vc1.php
@@ -77,7 +77,7 @@ include "function1.php";
         echo color("yellow",".");
         sleep(10);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"MAKANGOFOOD2107"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOJEKAMAN"}');
         $message = fetch_value($code1,'"message":"','"');
         echo "\n".color("green","+] Message: ".$message);
         sleep(2);
